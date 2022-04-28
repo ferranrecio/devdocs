43a2ad1ec040eb2ae886ab3c6bde51b1b5aac0b4
Ferran Recio <ferran@moodle.com>
2022-04-28 17:11:29 +0200

    [docs] Migrate plugintypes/format docs

---
+++ docs/apis/plugintypes/format/_examples/format_lang.php
@@ -0,0 +1,35 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component [pluginname] course format.
+ *
+ * @package   format_[pluginname]
+ * @copyright 2022 Someone <someone@somewhere.com>
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['addsections'] = 'Add section';
+$string['currentsection'] = 'This section';
+$string['deletesection'] = 'Delete section';
+$string['editsection'] = 'Edit section';
+$string['editsectionname'] = 'Edit section name';
+$string['hidefromothers'] = 'Hide section';
+$string['newsectionname'] = 'New name for section {$a}';
+$string['pluginname'] = '[pluginname] format';
+$string['privacy:metadata'] = 'The Sample format plugin does not store any personal data.';
+$string['sectionname'] = 'Section';
+$string['showfromothers'] = 'Show section';
